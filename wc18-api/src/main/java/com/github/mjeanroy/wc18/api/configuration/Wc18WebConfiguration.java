f54be36db25ebec6ecf2857beb348b082b2b105f
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2018-05-17 15:00:28 +0200

    chore: force default timezone to UTC

--- wc18-api/src/main/java/com/github/mjeanroy/wc18/api/configuration/Wc18WebConfiguration.java
+++ wc18-api/src/main/java/com/github/mjeanroy/wc18/api/configuration/Wc18WebConfiguration.java
@@ -6,9 +6,14 @@
 
 package com.github.mjeanroy.wc18.api.configuration;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @Configuration
 @Import({
@@ -21,4 +26,15 @@ import org.springframework.context.annotation.PropertySource;
 		"classpath:hibernate.properties"
 })
 public class Wc18WebConfiguration {
+
+	/**
+	 * Class Logger.
+	 */
+	private static final Logger log = LoggerFactory.getLogger(Wc18WebConfiguration.class);
+
+	@PostConstruct
+	public void setUp() {
+		log.info("Force default time-zone to UTC");
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
 }
