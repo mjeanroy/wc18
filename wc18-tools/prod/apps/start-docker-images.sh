91276bfef5156607b9a1cfefdc0398ad38489340
Mickael Jeanroy <mickael.jeanroy@gmail.com>
2018-06-10 22:14:05 +0200

    chore: add traefik configuration

--- wc18-tools/prod/apps/start-docker-images.sh
+++ wc18-tools/prod/apps/start-docker-images.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ##
 # Copyright (C) Mickael Jeanroy - All Rights Reserved.
 # Unauthorized copying of this file, via any medium is strictly prohibited.
---
+++ wc18-tools/prod/traefik/start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "[debug] Starting traefik reverse proxy"
+echo "[debug] Using PWD: ${PWD}"
+
+docker run -d \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v ${PWD}/traefik.toml:/traefik.toml \
+  -v ${PWD}/acme.json:/acme.json \
+  -p 80:80 \
+  -p 443:443 \
+  -l traefik.frontend.rule=Host:traefik.wc18.mjeanroy.io \
+  -l traefik.port=8080 \
+  --network wc18_network \
+  --name traefik \
+  traefik:1.6.3-alpine --docker
